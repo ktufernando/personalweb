3e829203fd4c83e9aa5e322e338f843f55beb771
Fernando Valdes <fernandoluis.valdes@bbva.com>
2019-10-04 17:44:23 -0300

    first commit

--- config/theme.js
+++ config/theme.js
@@ -1,8 +1,8 @@
 import { lighten } from 'polished'
 
 const colors = {
-  primary: '#d02e77', // Color for buttons or links
-  primaryLight: lighten(0.05, '#d02e77'),
+  primary: '#2e82d0', // Color for buttons or links
+  primaryLight: lighten(0.05, '#2e82d0'),
   bg: 'white', // Background color
   grey: {
     dark: 'rgba(0, 0, 0, 0.9)',
--- src/pages/index.js
+++ src/pages/index.js
@@ -40,6 +40,12 @@ const Hero = styled.div`
     }
   }
 `
+const SocialDiv = styled.div`
+  margin: 20px 0 20px 0;
+`
+const SocialIcon = styled.a`
+  padding: 0 3px 0 0;
+`
 
 const IndexPage = ({
   data: {
@@ -49,17 +55,43 @@ const IndexPage = ({
   <Layout>
     <Wrapper>
       <Hero>
-        <h1>Hi.</h1>
+        <h1>Hola.</h1>
         <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
+          Soy Fernando Valdes, desarrollador java y node desde hace algunos años. Hoy me contratan como arquitecto de soluciones.
         </p>
+        <br/>
+        <p>
+          Aquí vas a poder encontrar artículos que escribí sobre desarrollo personal y profesional orientado a la construcción de aplicaciones y servicios.
+        </p>
+        
+        <SocialDiv>
+          <SocialIcon>
+            <a href="https://www.linkedin.com/in/fervaldes/">
+              <img src="social/linkedin.svg" alt="Linkedin" width="45px" height="45px" />
+            </a>
+          </SocialIcon>
+          <SocialIcon>
+            <a href="https://github.com/ktufernando">
+              <img src="social/github.svg" alt="Github" width="45px" height="45px" />
+            </a>
+          </SocialIcon>
+          <SocialIcon>
+            <a href="https://www.instagram.com/ktufernando/">
+              <img src="social/instagram.svg" alt="Instagram" width="45px" height="45px" />
+            </a>
+          </SocialIcon>
+          <SocialIcon>
+            <a href="https://medium.com/@ktufernando/">
+              <img src="social/medium.svg" alt="Medium" width="45px" height="45px" />
+            </a>
+          </SocialIcon>
+        </SocialDiv>
         <Link to="/contact">
           <Button big>
             <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
               <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
             </svg>
-            Contact
+            Mandame un mensaje!
           </Button>
         </Link>
       </Hero>
